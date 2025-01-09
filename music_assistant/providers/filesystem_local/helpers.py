bf39235ac6c708a4618f5a21a09c52fd4a0b84c1
John Carr <john.carr@unrouted.co.uk>
2025-01-09 14:30:46 +0000

    Merge mypy.ini back into pyproject.toml

--- music_assistant/providers/filesystem_local/helpers.py
+++ music_assistant/providers/filesystem_local/helpers.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import os
 import re
 from dataclasses import dataclass
+from typing import AnyStr
 
 from music_assistant.helpers.compare import compare_strings
 
@@ -61,7 +62,7 @@ class FileSystemItem:
         return os.path.dirname(self.relative_path)
 
     @classmethod
-    def from_dir_entry(cls, entry: os.DirEntry, base_path: str) -> FileSystemItem:
+    def from_dir_entry(cls, entry: os.DirEntry[str], base_path: str) -> FileSystemItem:
         """Create FileSystemItem from os.DirEntry. NOT Async friendly."""
         if entry.is_dir(follow_symlinks=False):
             return cls(
