a908a7fb4fdfbb0db54d48c08888db917b5b38a6
Fabian Munkes <105975993+fmunkes@users.noreply.github.com>
2025-01-13 22:21:17 +0100

    use mashumaro lib for schema

--- music_assistant/providers/audiobookshelf/abs_client.py
+++ music_assistant/providers/audiobookshelf/abs_client.py
@@ -11,19 +11,12 @@ from aiohttp import ClientSession
 
 from music_assistant.providers.audiobookshelf.abs_schema import (
     ABSAudioBook,
-    ABSAudioBookChapter,
-    ABSAudioBookmark,
-    ABSAudioBookMedia,
-    ABSAudioBookMetaData,
-    ABSAudioTrack,
-    ABSAuthorMinified,
+    ABSLibrariesItemsResponse,
+    ABSLibrariesResponse,
     ABSLibrary,
+    ABSLoginResponse,
     ABSMediaProgress,
     ABSPodcast,
-    ABSPodcastEpisodeExpanded,
-    ABSPodcastMedia,
-    ABSPodcastMetaData,
-    ABSSeriesSequence,
     ABSUser,
 )
 
@@ -82,7 +75,7 @@ class ABSClient:
         endpoint: str,
         data: dict[str, Any] | None = None,
         add_api_endpoint: bool = True,
-    ) -> dict[str, Any]:
+    ) -> bytes:
         """POST request to abs api.
 
         login and logout endpoint do not have "api" in url
@@ -96,11 +89,9 @@ class ABSClient:
         status = response.status
         if status != ABSStatus.STATUS_OK.value:
             raise RuntimeError(f"API post call to {endpoint=} failed.")
-        return await response.json()
+        return await response.read()
 
-    async def _get(
-        self, endpoint: str, params: dict[str, str | int] | None = None
-    ) -> dict[str, Any]:
+    async def _get(self, endpoint: str, params: dict[str, str | int] | None = None) -> bytes:
         """GET request to abs api."""
         _endpoint = f"{self.base_url}/api/{endpoint}"
         response = await self.session.get(
@@ -110,9 +101,9 @@ class ABSClient:
         if status not in [ABSStatus.STATUS_OK.value, ABSStatus.STATUS_NOT_FOUND.value]:
             raise RuntimeError(f"API get call to {endpoint=} failed.")
         if response.content_type == "application/json":
-            return await response.json()
+            return await response.read()
         elif status == ABSStatus.STATUS_NOT_FOUND.value:
-            return {}
+            return b""
         else:
             raise RuntimeError("Response must be json.")
 
@@ -134,7 +125,7 @@ class ABSClient:
             data={"username": username, "password": password},
         )
 
-        return await self._parse_user(data["user"])
+        return ABSLoginResponse.from_json(data).user
 
     async def logout(self) -> None:
         """Logout and close aiohttp session."""
@@ -144,38 +135,21 @@ class ABSClient:
     async def get_user(self, id_: str) -> ABSUser:
         """Get an ABS user."""
         data = await self._get(f"users/{id_}")
-        return await self._parse_user(data)
-
-    async def _parse_user(self, user_dict: dict[str, Any]) -> ABSUser:
-        """Convert User Dict to ABSUser."""
-        abs_user = await self._map_attributes(ABSUser, user_dict)
-
-        abs_user.media_progress = []
-        for mp in user_dict["mediaProgress"]:
-            abs_mp = await self._map_attributes(ABSMediaProgress, mp)
-            abs_user.media_progress.append(abs_mp)
-
-        abs_user.bookmarks = []
-        for bm in user_dict["bookmarks"]:
-            abs_bm = await self._map_attributes(ABSAudioBookmark, bm)
-            abs_user.bookmarks.append(abs_bm)
-
-        return abs_user
+        return ABSUser.from_json(data)
 
     async def sync(self) -> None:
         """Update available book and podcast libraries."""
         data = await self._get("libraries")
-        libraries = data["libraries"]
+        libraries = ABSLibrariesResponse.from_json(data)
         ids = [x.id_ for x in self.audiobook_libraries]
         ids.extend([x.id_ for x in self.podcast_libraries])
-        for library in libraries:
-            media_type = library["mediaType"]
-            if library["id"] not in ids:
-                abs_library = ABSLibrary(name=library["name"], id_=library["id"])
+        for library in libraries.libraries:
+            media_type = library.media_type
+            if library.id_ not in ids:
                 if media_type == "book":
-                    self.audiobook_libraries.append(abs_library)
+                    self.audiobook_libraries.append(library)
                 elif media_type == "podcast":
-                    self.podcast_libraries.append(abs_library)
+                    self.podcast_libraries.append(library)
         self.user = await self.get_user(self.user.id_)
 
     async def get_all_podcasts(self) -> AsyncGenerator[ABSPodcast]:
@@ -198,13 +172,13 @@ class ABSClient:
     async def get_all_podcasts_by_library(self, lib: ABSLibrary) -> AsyncGenerator[ABSPodcast]:
         """Get all podcasts in a library."""
         async for podcast_data in self._get_lib_items(lib):
-            podcast_list = podcast_data.get("results", None)
+            podcast_list = ABSLibrariesItemsResponse.from_json(podcast_data).results
             if not podcast_list:  # [] if page exceeds
                 return
 
             async def _get_id(plist=podcast_list):
                 for entry in plist:
-                    yield entry["id"]
+                    yield entry.id_
 
             async for id_ in _get_id():
                 podcast = await self.get_podcast(id_)
@@ -213,26 +187,8 @@ class ABSClient:
     async def get_podcast(self, id_: str) -> ABSPodcast:
         """Get a single Podcast by ID."""
         # this endpoint gives more podcast extra data
-        podcast = await self._get(f"items/{id_}?expanded=1")
-        abs_podcast = await self._map_attributes(ABSPodcast, podcast)
-
-        abs_media = await self._map_attributes(ABSPodcastMedia, podcast["media"])
-        abs_podcast.media = abs_media
-
-        abs_metadata = await self._map_attributes(ABSPodcastMetaData, podcast["media"]["metadata"])
-        abs_media.metadata = abs_metadata
-
-        abs_podcast.media.episodes = []
-        for episode in podcast["media"]["episodes"]:
-            abs_episode = await self._map_attributes(ABSPodcastEpisodeExpanded, episode)
-            abs_episode.audio_track = await self._map_attributes(
-                ABSAudioTrack, episode["audioTrack"]
-            )
-
-            abs_podcast.media.episodes.append(abs_episode)
-        abs_podcast.media.num_episodes = len(abs_podcast.media.episodes)
-
-        return abs_podcast
+        data = await self._get(f"items/{id_}?expanded=1")
+        return ABSPodcast.from_json(data)
 
     async def _get_progress_ms(
         self,
@@ -242,7 +198,7 @@ class ABSClient:
         if not data:
             # entry doesn't exist, so it wasn't played yet
             return 0, False
-        abs_media_progress: ABSMediaProgress = await self._map_attributes(ABSMediaProgress, data)
+        abs_media_progress = ABSMediaProgress.from_json(data)
 
         return (
             int(abs_media_progress.current_time * 1000),
@@ -327,13 +283,13 @@ class ABSClient:
     async def get_all_audiobooks_by_library(self, lib: ABSLibrary) -> AsyncGenerator[ABSAudioBook]:
         """Get all Audiobooks in a library."""
         async for audiobook_data in self._get_lib_items(lib):
-            audiobook_list = audiobook_data.get("results", None)
+            audiobook_list = ABSLibrariesItemsResponse.from_json(audiobook_data).results
             if not audiobook_list:  # [] if page exceeds
                 return
 
             async def _get_id(alist=audiobook_list):
                 for entry in alist:
-                    yield entry["id"]
+                    yield entry.id_
 
             async for id_ in _get_id():
                 audiobook = await self.get_audiobook(id_)
@@ -343,45 +299,4 @@ class ABSClient:
         """Get a single Audiobook by ID."""
         # this endpoint gives more audiobook extra data
         audiobook = await self._get(f"items/{id_}?expanded=1")
-        abs_audiobook = await self._map_attributes(ABSAudioBook, audiobook)
-
-        abs_media = await self._map_attributes(ABSAudioBookMedia, audiobook["media"])
-        abs_audiobook.media = abs_media
-
-        abs_metadata = await self._map_attributes(
-            ABSAudioBookMetaData, audiobook["media"]["metadata"]
-        )
-        abs_media.metadata = abs_metadata
-
-        abs_metadata.authors = []
-        for author in audiobook["media"]["metadata"]["authors"]:
-            abs_author = await self._map_attributes(ABSAuthorMinified, author)
-            abs_metadata.authors.append(abs_author)
-
-        abs_metadata.series = []
-        for series in audiobook["media"]["metadata"]["series"]:
-            abs_series = await self._map_attributes(ABSSeriesSequence, series)
-            abs_metadata.series.append(abs_series)
-
-        abs_media.chapters = []
-        for chapter in audiobook["media"]["chapters"]:
-            abs_chapter = await self._map_attributes(ABSAudioBookChapter, chapter)
-            abs_media.chapters.append(abs_chapter)
-
-        abs_media.tracks = []
-        for track in audiobook["media"]["tracks"]:
-            abs_audio_track = await self._map_attributes(ABSAudioTrack, track)
-            abs_media.tracks.append(abs_audio_track)
-
-        return abs_audiobook
-
-    async def _map_attributes(self, dclass, api_dict: dict[str, Any]):
-        # how do I type hint this function...
-        abs_class = dclass()
-        for key, value in api_dict.items():
-            related_key = camel_to_snake(key)
-            try:
-                setattr(abs_class, related_key, value)
-            except AttributeError:
-                continue
-        return abs_class
+        return ABSAudioBook.from_json(audiobook)
--- music_assistant/providers/audiobookshelf/abs_schema.py
+++ music_assistant/providers/audiobookshelf/abs_schema.py
@@ -4,123 +4,133 @@ https://api.audiobookshelf.org/
 """
 
 from dataclasses import dataclass
+from typing import Annotated
+
+from mashumaro.config import BaseConfig
+from mashumaro.mixins.json import DataClassJSONMixin
+from mashumaro.types import Alias
 
 
-class ABSSchema:
-    """For Type Hinting."""
+class BaseModel(DataClassJSONMixin):
+    """BaseModel for Schema part where we don't need all keys."""
+
+    class Config(BaseConfig):
+        """Not all keys required."""
+
+        forbid_extra_keys = False
 
 
-@dataclass(init=False)
-class ABSAudioTrack(ABSSchema):
+@dataclass
+class ABSAudioTrack(BaseModel):
     """ABS audioTrack.
 
     https://api.audiobookshelf.org/#audio-track
     """
 
     index: int
-    start_offset: float = 0.0
+    start_offset: Annotated[float, Alias("startOffset")] = 0.0
     duration: float = 0.0
     title: str = ""
-    content_url: str = ""
+    content_url: Annotated[str, Alias("contentUrl")] = ""
     mime_type: str = ""
     # metadata: # not needed for mass application
 
 
-@dataclass(init=False)
-class ABSPodcastEpisodeExpanded(ABSSchema):
+@dataclass
+class ABSPodcastEpisodeExpanded(BaseModel):
     """ABSPodcastEpisode.
 
     https://api.audiobookshelf.org/#podcast-episode
     """
 
-    library_item_id: str
-    id_: str
-    index: int
+    library_item_id: Annotated[str, Alias("libraryItemId")]
+    id_: Annotated[str, Alias("id")]
+    index: int | None
     # audio_file: # not needed for mass application
-    published_at: int  # ms posix epoch
-    added_at: int  # ms posix epoch
-    updated_at: int  # ms posix epoch
-    audio_track: ABSAudioTrack
+    published_at: Annotated[int | None, Alias("publishedAt")]  # ms posix epoch
+    added_at: Annotated[int | None, Alias("addedAt")]  # ms posix epoch
+    updated_at: Annotated[int | None, Alias("updatedAt")]  # ms posix epoch
+    audio_track: Annotated[ABSAudioTrack, Alias("audioTrack")]
     size: int  # in bytes
     season: str = ""
     episode: str = ""
-    episode_type: str = ""
+    episode_type: Annotated[str, Alias("episodeType")] = ""
     title: str = ""
     subtitle: str = ""
     description: str = ""
     enclosure: str = ""
-    pub_date: str = ""
+    pub_date: Annotated[str, Alias("pubDate")] = ""
     guid: str = ""
     # chapters
     duration: float = 0.0
 
 
-@dataclass(init=False)
-class ABSPodcastMetaData(ABSSchema):
+@dataclass
+class ABSPodcastMetaData(BaseModel):
     """PodcastMetaData https://api.audiobookshelf.org/?shell#podcasts."""
 
     title: str | None
     author: str | None
     description: str | None
-    release_date: str | None
+    release_date: Annotated[str | None, Alias("releaseDate")]
     genres: list[str]
-    feed_url: str | None
-    image_url: str | None
-    itunes_page_url: str | None
-    itunes_id: int | None
-    itunes_artist_id: int | None
+    feed_url: Annotated[str | None, Alias("feedUrl")]
+    image_url: Annotated[str | None, Alias("imageUrl")]
+    itunes_page_url: Annotated[str | None, Alias("itunesPageUrl")]
+    itunes_id: Annotated[int | None, Alias("itunesId")]
+    itunes_artist_id: Annotated[int | None, Alias("itunesArtistId")]
     explicit: bool
     language: str | None
-    type_: str | None
+    type_: Annotated[str | None, Alias("type")]
 
 
-@dataclass(init=False)
-class ABSPodcastMedia(ABSSchema):
+@dataclass
+class ABSPodcastMedia(BaseModel):
     """ABSPodcastMedia."""
 
     metadata: ABSPodcastMetaData
-    cover_path: str
+    cover_path: Annotated[str, Alias("coverPath")]
     episodes: list[ABSPodcastEpisodeExpanded]
-    num_episodes: int = 0
+    num_episodes: Annotated[int, Alias("numEpisodes")] = 0
 
 
-@dataclass(init=False)
-class ABSPodcast(ABSSchema):
+@dataclass
+class ABSPodcast(BaseModel):
     """ABSPodcast.
 
     Depending on endpoint we get different results. This class does not
     fully reflect https://api.audiobookshelf.org/#podcast.
     """
 
-    id_: str
+    id_: Annotated[str, Alias("id")]
     media: ABSPodcastMedia
 
 
-@dataclass(init=False)
-class ABSAuthorMinified(ABSSchema):
+@dataclass
+class ABSAuthorMinified(BaseModel):
     """ABSAuthor.
 
     https://api.audiobookshelf.org/#author
     """
 
-    id_: str
+    id_: Annotated[str, Alias("id")]
     name: str
 
 
-@dataclass(init=False)
-class ABSSeriesSequence(ABSSchema):
+@dataclass
+class ABSSeriesSequence(BaseModel):
     """Series Sequence.
 
     https://api.audiobookshelf.org/#series
     """
 
-    id_: str
+    id_: Annotated[str, Alias("id")]
     name: str
     sequence: str | None
 
 
-@dataclass(init=False)
-class ABSAudioBookMetaData(ABSSchema):
+@dataclass
+class ABSAudioBookMetaData(BaseModel):
     """ABSAudioBookMetaData.
 
     https://api.audiobookshelf.org/#book-metadata
@@ -132,8 +142,8 @@ class ABSAudioBookMetaData(ABSSchema):
     narrators: list[str]
     series: list[ABSSeriesSequence]
     genres: list[str]
-    published_year: str | None
-    published_date: str | None
+    published_year: Annotated[str | None, Alias("publishedYear")]
+    published_date: Annotated[str | None, Alias("publishedDate")]
     publisher: str | None
     description: str | None
     isbn: str | None
@@ -142,97 +152,165 @@ class ABSAudioBookMetaData(ABSSchema):
     explicit: bool
 
 
-@dataclass(init=False)
-class ABSAudioBookChapter(ABSSchema):
+@dataclass
+class ABSAudioBookChapter(BaseModel):
     """
     ABSAudioBookChapter.
 
     https://api.audiobookshelf.org/#book-chapter
     """
 
-    id_: int
+    id_: Annotated[int, Alias("id")]
     start: float
     end: float
     title: str
 
 
-@dataclass(init=False)
-class ABSAudioBookMedia(ABSSchema):
+@dataclass
+class ABSAudioBookMedia(BaseModel):
     """ABSAudioBookMedia.
 
     Helper class due to API endpoint used.
     """
 
     metadata: ABSAudioBookMetaData
-    cover_path: str
+    cover_path: Annotated[str, Alias("coverPath")]
     chapters: list[ABSAudioBookChapter]
     duration: float
     tracks: list[ABSAudioTrack]
 
 
-@dataclass(init=False)
-class ABSAudioBook(ABSSchema):
+@dataclass
+class ABSAudioBook(BaseModel):
     """ABSAudioBook.
 
     Depending on endpoint we get different results. This class does not
     full reflect https://api.audiobookshelf.org/#book.
     """
 
-    id_: str
+    id_: Annotated[str, Alias("id")]
     media: ABSAudioBookMedia
 
 
-@dataclass(init=False)
-class ABSMediaProgress(ABSSchema):
+@dataclass
+class ABSMediaProgress(BaseModel):
     """ABSMediaProgress.
 
     https://api.audiobookshelf.org/#media-progress
     """
 
-    id_: str
-    library_item_id: str
-    episode_id: str
+    id_: Annotated[str, Alias("id")]
+    library_item_id: Annotated[str, Alias("libraryItemId")]
+    episode_id: Annotated[str, Alias("episodeId")]
     duration: float  # seconds
     progress: float  # percent 0->1
-    current_time: float  # seconds
-    is_finished: bool
-    hide_from_continue_listening: bool
-    last_update: int  # ms epoch
-    started_at: int  # ms epoch
-    finished_at: int  # ms epoch
+    current_time: Annotated[float, Alias("currentTime")]  # seconds
+    is_finished: Annotated[bool, Alias("isFinished")]
+    hide_from_continue_listening: Annotated[bool, Alias("hideFromContinueListening")]
+    last_update: Annotated[int, Alias("lastUpdate")]  # ms epoch
+    started_at: Annotated[int, Alias("startedAt")]  # ms epoch
+    finished_at: Annotated[int | None, Alias("finishedAt")]  # ms epoch
 
 
-@dataclass(init=False)
-class ABSAudioBookmark(ABSSchema):
+@dataclass
+class ABSAudioBookmark(BaseModel):
     """ABSAudioBookmark."""
 
-    library_item_id: str
+    library_item_id: Annotated[str, Alias("libraryItemId")]
     title: str
     time: float  # seconds
-    created_at: int  # unix epoch ms
+    created_at: Annotated[int, Alias("createdAt")]  # unix epoch ms
 
 
-@dataclass(init=False)
-class ABSUser(ABSSchema):
+@dataclass
+class ABSUserPermissions(BaseModel):
+    """ABSUserPermissions."""
+
+    download: bool
+    update: bool
+    delete: bool
+    upload: bool
+    access_all_libraries: Annotated[bool, Alias("accessAllLibraries")]
+    access_all_tags: Annotated[bool, Alias("accessAllTags")]
+    access_explicit_content: Annotated[bool, Alias("accessExplicitContent")]
+
+
+@dataclass
+class ABSUser(BaseModel):
     """ABSUser.
 
     only attributes we need for mass
     https://api.audiobookshelf.org/#user
     """
 
-    id_: str
-    token: str
+    id_: Annotated[str, Alias("id")]
     username: str
-    media_progress: list[ABSMediaProgress]
+    type_: Annotated[str, Alias("type")]
+    token: str
+    media_progress: Annotated[list[ABSMediaProgress], Alias("mediaProgress")]
+    series_hide_from_continue_listening: Annotated[
+        list[str], Alias("seriesHideFromContinueListening")
+    ]
     bookmarks: list[ABSAudioBookmark]
+    is_active: Annotated[bool, Alias("isActive")]
+    is_locked: Annotated[bool, Alias("isLocked")]
+    last_seen: Annotated[int | None, Alias("lastSeen")]
+    created_at: Annotated[int, Alias("createdAt")]
+    permissions: ABSUserPermissions
+    libraries_accessible: Annotated[list[str], Alias("librariesAccessible")]
+
+    # this seems to be missing
+    # item_tags_accessible: Annotated[list[str], Alias("itemTagsAccessible")]
 
 
 @dataclass
-class ABSLibrary(ABSSchema):
+class ABSLoginResponse(BaseModel):
+    """ABSLoginResponse."""
+
+    user: ABSUser
+
+    # this seems to be missing
+    # user_default_library_id: Annotated[str, Alias("defaultLibraryId")]
+
+
+@dataclass
+class ABSLibrary(BaseModel):
     """ABSLibrary.
 
     Only attributes we need
     """
 
-    id_: str
+    id_: Annotated[str, Alias("id")]
     name: str
+    # folders
+    # displayOrder: Integer
+    # icon: String
+    media_type: Annotated[str, Alias("mediaType")]
+    provider: str
+    # settings
+    created_at: Annotated[int, Alias("createdAt")]
+    last_update: Annotated[int, Alias("lastUpdate")]
+
+
+@dataclass
+class ABSLibrariesResponse(BaseModel):
+    """ABSLibrariesResponse."""
+
+    libraries: list[ABSLibrary]
+
+
+@dataclass
+class ABSLibraryItem(BaseModel):
+    """ABSLibraryItem."""
+
+    id_: Annotated[str, Alias("id")]
+
+
+@dataclass
+class ABSLibrariesItemsResponse(BaseModel):
+    """ABSLibrariesItemsResponse.
+
+    https://api.audiobookshelf.org/#get-a-library-39-s-items
+    """
+
+    results: list[ABSLibraryItem]
