55cbb20139ef4e249707be5db15e270cbb4ee6cd
Maxim Raznatovski <nda.mr43@gmail.com>
2025-02-15 16:48:59 +0100

    refactor: early return

--- music_assistant/providers/player_group/__init__.py
+++ music_assistant/providers/player_group/__init__.py
@@ -902,28 +902,30 @@ class PlayerGroupProvider(PlayerProvider):
         group_type = self.mass.config.get_raw_player_config_value(
             player.player_id, CONF_ENTRY_GROUP_TYPE.key, CONF_ENTRY_GROUP_TYPE.default_value
         )
-        if group_type == GROUP_TYPE_UNIVERSAL:
-            changed = False
-            # Verify that no player is part of a separate group
-            for child_player_id in player.group_childs:
-                child_player = self.mass.players.get(child_player_id)
-                if PlayerFeature.SET_MEMBERS not in child_player.supported_features:
-                    continue
-                if child_player.group_childs:
-                    # This is a leader in another group
-                    player_provider = self.mass.players.get_player_provider(child_player_id)
-                    for sync_child_id in child_player.group_childs:
-                        if sync_child_id == child_player_id:
-                            continue
-                        await player_provider.cmd_ungroup(sync_child_id)
-                        changed = True
-                if child_player.synced_to:
-                    # This is a member of another group
-                    await self.cmd_ungroup_member(child_player.player_id, child_player.synced_to)
+        if group_type != GROUP_TYPE_UNIVERSAL:
+            return
+
+        changed = False
+        # Verify that no player is part of a separate group
+        for child_player_id in player.group_childs:
+            child_player = self.mass.players.get(child_player_id)
+            if PlayerFeature.SET_MEMBERS not in child_player.supported_features:
+                continue
+            if child_player.group_childs:
+                # This is a leader in another group
+                player_provider = self.mass.players.get_player_provider(child_player_id)
+                for sync_child_id in child_player.group_childs:
+                    if sync_child_id == child_player_id:
+                        continue
+                    await player_provider.cmd_ungroup(sync_child_id)
                     changed = True
-            if changed and player.state == PlayerState.PLAYING:
-                # Restart playback to ensure all members play the same content
-                await self.mass.player_queues.resume(player.player_id)
+            if child_player.synced_to:
+                # This is a member of another group
+                await self.cmd_ungroup_member(child_player.player_id, child_player.synced_to)
+                changed = True
+        if changed and player.state == PlayerState.PLAYING:
+            # Restart playback to ensure all members play the same content
+            await self.mass.player_queues.resume(player.player_id)
 
     async def _serve_ugp_stream(self, request: web.Request) -> web.Response:
         """Serve the UGP (multi-client) flow stream audio to a player."""
